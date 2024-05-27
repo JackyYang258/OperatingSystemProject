eda8b1064ff0faffe753ac538cbb80dd74edbcbe
JackyYang258 <1213853292@qq.com>
2024-05-27 19:33:11 +0800

    modify 1

--- 1BankService.cpp
+++ 1BankService.cpp
@@ -10,26 +10,28 @@ using namespace std;
 
 // define numbers
 const int counter_num = 3;
-const int maxnum_customer = 20;
+const int maxnum_customer = 20; // max num of customer in wait
 
 // define structures
+// input of customer
 struct customer_in
 {
-    int cus_no; // �˿ͱ��
-    int time_in; // ����ʱ��
-    int time_serve; // ��Ҫ�����ʱ�䳤��
+    int cus_no; 
+    int time_in; 
+    int time_serve; 
 };
 
 struct customer_out
 {
-    double time_in; // ����ʱ��
-    double time_beginserve; // ��ʼ�����ʱ��
-    int counter_NO; // ��̨��
-    double time_served; // ��������ʱ��
+    double time_in; 
+    double time_beginserve; 
+    int counter_NO; 
+    double time_served; // time when finish serve
 };
 
-int customer_num = 0; // �˿�����
+int customer_num = 0; 
 int get_num = 0;
+int queue_length = 0;
 
 vector <customer_in> customers_in;
 vector <customer_out> customers_out;
@@ -44,15 +46,24 @@ chrono::time_point<chrono::system_clock> time_begin = chrono::system_clock::now(
 
 // process of customer
 void Customer(int PV) {
-    this_thread::sleep_for(chrono::seconds(customers_in[PV].time_in)); // 
+    this_thread::sleep_for(chrono::seconds(customers_in[PV].time_in)); // wait for arrive 
 
-    // ռ��ȡ�Ż�
     mtx_get_num.lock();
 
     chrono::time_point<chrono::system_clock> time_in = chrono::system_clock::now();
     customers_out[PV].time_in = chrono::duration<double>(time_in - time_begin).count();
 
     get_num ++;
+    if (queue_length < maxnum_customer) {
+        queue_length ++;
+    }
+    else {
+        mtx_output.lock();
+        cout << "NO." << PV + 1 << " customer is leaving" << endl;
+        mtx_output.unlock();
+        mtx_get_num.unlock();
+        return;
+    }
     wait_queue.push(PV);
 
     mtx_output.lock();
@@ -102,7 +113,7 @@ int main() {
     }
     customers_out.resize(customer_num);
 
-    // �����˿��߳�
+    //start threads
     vector<thread> customer_threads;
     for (int j = 0; j < customer_num; j++) {
         customer_threads.push_back(thread(Customer, j));
@@ -113,7 +124,7 @@ int main() {
         counter_threads.push_back(thread(Counter, j));
     }
 
-    // �ȴ����й˿ͱ��������
+    // wait all threads to end
     for (int j = 0; j < customer_num; j++) {
         customer_threads[j].join();
     }
@@ -122,9 +133,9 @@ int main() {
         counter_threads[j].join();
     }
 
-    cout << "�����Ӵ������˿�����:" << maxnum_customer << endl;
-    cout << "ȫ��˿ͽӴ������" << endl;
-    cout << "�˿ͱ��\t����ʱ��\t��ʼʱ��\t��������ʱ��\t��̨��" << endl;
+    cout << "max number of customer:" << maxnum_customer << endl;
+    cout << "serve information" << endl;
+    cout << "No\ttime_in\tserve_start\tserve_end\tcounter NO." << endl;
 
     // ����˿ͽӴ����
     for (int k = 0; k < customer_num; k++) {
